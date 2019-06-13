bea9894d39177623bf4fdb4db46aef90e9584f88
Eugene R <2contact.er@gmail.com>
2019-06-13 16:03:53 -0400

    initial revision

---
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,93 @@
+package com.example.reservationservice;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	@Bean
+	CommandLineRunner commandLineRunner(ReservationRepository reservationRepository) {
+		return names -> {
+			Stream.of("Mike", "Bob", "Joshua", "Max")
+					.forEach(name -> reservationRepository.save(new Reservation(name)));
+		};
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+	@RestResource(path = "by-name")
+	Collection<Reservation> findByReservationName(String rn);
+}
+
+@Entity
+class Reservation {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String reservationName;
+
+	public Reservation(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	public Reservation() { // default constructor is needed for JPA
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", reservationName=" + reservationName + "]";
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+	@Value("${message}") // the value will be taken from config server: 
+	private String msg;
+
+	@RequestMapping("/message")
+	String message() {
+		return this.msg;
+	}
+
+}
