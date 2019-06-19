dae0f035d3b62a17981a635fc6734ab7f8ed858f
Eugene R <2contact.er@gmail.com>
2019-06-19 07:39:59 -0400

    update

---
+++ openid-sso-client/src/main/java/com/example/openidssoclient/OpenidSsoClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.openidssoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenidSsoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenidSsoClientApplication.class, args);
+	}
+
+}
